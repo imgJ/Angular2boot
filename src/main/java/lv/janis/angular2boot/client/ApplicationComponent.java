597c0955bb971dd967f83c3fcabc0bec40902518
Janis Karklins <jjankar@gmail.com>
2016-11-16 13:21:01 +0200

    HTML for list is added

--- src/main/java/lv/janis/angular2boot/client/ApplicationComponent.java
+++ src/main/java/lv/janis/angular2boot/client/ApplicationComponent.java
@@ -13,9 +13,16 @@ import jsinterop.annotations.JsType;
 @Component(
         selector = "my-app",
         template = "<h1>{{title}}</h1>"
+        + "<h2>My Heroes</h2>"
+        + "<ul class='heroes'>"
+        + "<li *ngFor='let hero of heroes'>"
+        + "<span class='badge'>{{hero.id}}</span> {{hero.name}}"
+        + "</li>"
+        + "</ul>"
         + "<h2>{{hero.name}} details!</h2>"
         + "<div><label>id: </label>{{hero.id}}</div>"
-        + "<div><label>name: </label><input [(ngModel)]='hero.name' placeholder='name'></div>")
+        + "<div><label>name: </label><input [(ngModel)]='hero.name' placeholder='name'></div>",
+        styleUrls = "application.component.css")
 @JsType
 public class ApplicationComponent {
 
